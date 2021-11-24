f6a2c988cbe49dd69d94ac31857df4ef8fadb1bc
HGdawy <hussien.gdawy@gmail.com>
2021-11-24 12:58:17 +0200

    add project

---
+++ src/client/index.js
@@ -0,0 +1,16 @@
+
+import { handleSubmit } from './js/formHandler'
+import { checkUrl } from './js/checkURL'
+
+// import sass files
+
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
+//exported to be a part of the client library 
+export {
+    handleSubmit,
+    checkUrl
+}
